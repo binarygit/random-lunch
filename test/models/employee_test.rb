0f8d48884efbcd74338f429a496ba5059a7071f2
binarygit <inscription4@outlook.com>
2023-02-10 12:33:23 +0545

    Test the employee model

--- test/models/employee_test.rb
+++ test/models/employee_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
 class EmployeeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @employee = Employee.new(email: "ava@ava.com", username: "ava")
+  end
+
+  test 'should be valid' do
+    assert @employee.valid?
+  end
+
+  test 'email should be present' do
+    @employee.email = ''
+    assert_not @employee.valid?
+  end
+
+  test 'username should be present' do
+    @employee.username = ''
+    assert_not @employee.valid?
+  end
+  
+  test 'email address and username should be unique' do
+    duplicate_user = @employee.dup
+    @employee.save
+    assert_not duplicate_user.valid?
+  end
 end
