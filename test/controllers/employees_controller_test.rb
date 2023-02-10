9f45e3d6aebd42098fc3e67cf2652770d0df818c
binarygit <inscription4@outlook.com>
2023-02-10 12:28:08 +0545

    Fix employees and static pages controller tests

--- test/controllers/employees_controller_test.rb
+++ test/controllers/employees_controller_test.rb
@@ -2,17 +2,7 @@ require "test_helper"
 
 class EmployeesControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get employees_new_url
-    assert_response :success
-  end
-
-  test "should get create" do
-    get employees_create_url
-    assert_response :success
-  end
-
-  test "should get index" do
-    get employees_index_url
+    get new_employee_url
     assert_response :success
   end
 end
