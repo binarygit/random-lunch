38b5ef619538dd509c8bd8902d670d8905ce0f7c
binarygit <inscription4@outlook.com>
2023-04-05 23:04:37 +0545

    Setup seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Employee.destroy_all
+
+Employee.create!(first_name: "Avi", last_name: "Bhurtel", username: "avi", email: "avi@avi.com", password: "foobarfoobar", password_confirmation: "foobarfoobar")
+
+8.times do
+  attrs = {first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, 
+           password: "foobarfoobar", password_confirmation: "foobarfoobar"}
+  Employee.create!(attrs.merge(Faker::Internet.unique.user))
+end
